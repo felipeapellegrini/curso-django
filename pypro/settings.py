715737466b2511c4063f283aaed2e8b71e1d0d18
Felipe de Almeida <felipeapellegrini@gmail.com>
2021-08-14 12:05:54 -0300

    chore: isolate secret

--- pypro/settings.py
+++ pypro/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i=l0xac@#idfj2v$i-x-w3xik5p2+!*_hs0yy7%4x-pd93=)#1'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
