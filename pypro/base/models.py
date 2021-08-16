1a4017ef42883e3bf7d882efdaa8c1e9fca567db
Felipe de Almeida <felipeapellegrini@gmail.com>
2021-08-16 17:19:44 -0300

    chore: config procfile

--- pypro/base/models.py
+++ pypro/base/models.py
@@ -75,7 +75,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     Email and password are required. Other fields are optional.
     """
- 
+
     first_name = models.CharField(_('first name'), max_length=150, blank=True)
     email = models.EmailField(_('email address'), unique=True)
     is_staff = models.BooleanField(
